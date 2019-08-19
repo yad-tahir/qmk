cc2044f41dc06e74bd2905952cf53f3c6810f7b7
Dr. Yad Tahir <yad@ieee.org>
2019-08-19 14:21:00 +0300

    Add my HHKB general settings

--- keyboards/hhkb/keymaps/evil/config.h
+++ keyboards/hhkb/keymaps/evil/config.h
@@ -3,13 +3,23 @@
 
 #include "../../config.h"
 
+// Device ID
 #undef MANUFACTURER
 #undef PRODUCT
 #undef DESCRIPTION
-#define MANUFACTURER    PFU
-#define PRODUCT         HHKB Evil
-#define DESCRIPTION     HHKB on QMK Firmware with evil keymap
 
-#define DYNAMIC_MACRO_SIZE 256
+#define MANUFACTURER QMK
+#define PRODUCT HHKB Evil
+#define DESCRIPTION HHKB on QMK Firmware with Evil keymap
+
+// Allocate memory for dynamic macros as much as possible
+#define DYNAMIC_MACRO_SIZE 300
+
+// Mouse config
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_INTERVAL 15
+#define MOUSEKEY_MAX_SPEED 2
+#define MOUSEKEY_TIME_TO_MAX 8
+#define MOUSEKEY_WHEEL_DELAY 0
 
 #endif
--- keyboards/hhkb/keymaps/evil/rules.mk
+++ keyboards/hhkb/keymaps/evil/rules.mk
@@ -1,4 +1,4 @@
-# cinaeco's HHKB firmware
+# Evil's HHKB firmware
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in
@@ -10,7 +10,7 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
